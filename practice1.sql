dad5540cdd6b6970cc8da3eea3ba946e0692a8fa
areebaqamar021 <areebaqamar021@gmail.com>
2025-09-15 14:37:53 +0500

    format SQL queries for better readability

--- practice1.sql
+++ practice1.sql
@@ -1,8 +1,14 @@
 -- 1. Show first name, last name, and gender of patients whose gender is 'M'
 
-select
+SELECT
   first_name,
   last_name,
   gender
-from patients
-where gender = 'M'
+FROM patients
+WHERE gender = 'M'
+
+-- 2. Show first name and last name of patients who does not have allergies. (null)
+
+SELECT first_name, last_name 
+FROM patients
+WHERE allergies IS NULL
