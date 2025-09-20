68411da860b49079f6391b189e2ebcc602aa6fb5
areebaqamar021 <areebaqamar021@gmail.com>
2025-09-20 05:58:05 +0500

    add SQL query to show all columns for specific patient_ids

--- practice1.sql
+++ practice1.sql
@@ -61,3 +61,13 @@ WHERE height = (
     SELECT max(height)
     FROM patients
   )
+
+-- 10. Show all columns for patients who have one of the following patient_ids:
+-- 1,45,534,879,1000
+
+SELECT *
+FROM patients
+WHERE
+  patient_id in(1, 45, 534, 879, 1000)
+
+11. 
