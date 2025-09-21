ff8b6945f07158e688b2b94f93f90e8a66dd13de
areebaqamar021 <areebaqamar021@gmail.com>
2025-09-21 14:35:24 +0500

    fix formatting for allergies update query in practice1.sql

--- practice1.sql
+++ practice1.sql
@@ -27,9 +27,9 @@ WHERE weight >= 100 and weight <= 120
 
 -- 5. Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
 
-update patients
-set allergies = 'NKA'
-where allergies is NULL
+UPDATE patients
+SET allergies = 'NKA'
+WHERE allergies is NULL
 
 -- 6. Show first name and last name concatinated into one column to show their full name.
 
