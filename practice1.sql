b9c66716e175c825a6f04611ece1f355d40bbd85
areebaqamar021 <areebaqamar021@gmail.com>
2025-09-23 14:59:53 +0500

    add additional SQL queries for patient details and unique birth years in practice2.sql; format updates in practice1.sql

--- practice1.sql
+++ practice1.sql
@@ -1,3 +1,5 @@
+----------------- EASY QUESTIONS -------------------
+
 -- 1. Show first name, last name, and gender of patients whose gender is 'M'
 
 SELECT
--- practice2.sql
+++ practice2.sql
@@ -10,3 +10,22 @@ SELECT DISTINCT city
 FROM patients
 WHERE province_id = 'NS'
 
+-- 15. Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70
+
+SELECT first_name, last_name, birth_date
+FROM patients
+WHERE height > 160 and weight > 70
+
+-- 16. Write a query to find list of patients first_name, last_name, and allergies where allergies are not null and are from the city of 'Hamilton'
+
+SELECT first_name, last_name, allergies
+FROM patients
+WHERE allergies IS NOT NULL AND city = 'Hamilton'
+
+----------------- MEDIUM QUESTIONS -------------------
+
+-- 17. Show unique birth years from patients and order them by ascending.
+
+SELECT DISTINCT YEAR(birth_date) AS birth_year
+FROM patients
+ORDER BY birth_year
